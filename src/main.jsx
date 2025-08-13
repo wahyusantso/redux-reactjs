5a2e829193643a259e21ef854eb726fe73b4f7a9
wahyusantso <wahyusantozo36@gmail.com>
2025-08-13 10:29:00 +0700

    feat(63): Store

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { configureStore } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {}
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
