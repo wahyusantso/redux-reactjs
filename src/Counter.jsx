f6157ea499e5b580d011e70c85aa135c5f539b6d
wahyusantso <wahyusantozo36@gmail.com>
2025-08-18 11:53:35 +0700

    feat(65): Action

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,25 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./counterSlice";
 
 export default function Counter() {
     //menentukan state yang digunakan berdasarkan yang diregister di store.
     const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch(); //define dispatch
+
+    //call dispatch
+    function handleIncrement() {
+        dispatch(increment(5));
+    }
+
+    function handleDecrement() {
+        dispatch(decrement(5));
+    }
 
     return (
         <div>
             <h1>Counter: {counter}</h1>
+            <button onClick={handleIncrement} style={{ marginRight: "30px" }}>Increment Counter + 5</button>
+            <button onClick={handleDecrement}>Decrement Counter - 5</button>
         </div>
     )
 }
--- src/counterSlice.js
+++ src/counterSlice.js
@@ -2,6 +2,25 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
     name: "counter",
-    initialState: 24,
-    reducers: {}
+    initialState: 0,
+    reducers: {
+        //implementasi action didalam reducer.
+        increment: (state, action) => { //nilai dari action disimpan pada payload.
+            if (action.payload) { //di jalankan jika terdapat nilai pada payload. 
+                return state + action.payload;
+            } else {
+                return state + 1;
+            }
+        },
+        decrement: (state, action) => {
+            if (action.payload) {
+                return state - action.payload;
+            } else {
+                return state - 1;
+            }
+        }
+    }
 });
+
+//use destructuring
+export const {increment, decrement} = counterSlice.actions;
